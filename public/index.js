9a6bbd0b86c6c57e8e142258d3d696b5e28ca3e3
jpclark6 <jpclark6@gmail.com>
2019-03-14 10:40:52 -0600

    finishes lesson minus one error on delete due to cors

--- public/index.js
+++ public/index.js
@@ -1,11 +1,41 @@
 const getHedgehogs = () => {
   $('#hedgehog-info').html('');
 
-  fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites`)
-    .then(response => response.json())
-    .then(hedgehogs => appendHedgehogs(hedgehogs))
-    .catch(error => console.error({ error }));
+  fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites`) // make a get request to url
+    .then(response => response.json()) // after getting back the request turn the response into json format (object)
+    .then(hedgehogs => appendHedgehogs(hedgehogs)) // take the json, call it hedgehogs, and then run the function appendHedgehogs on the hedgehog array
+    .catch(error => console.error({ error }));  // if there's an error in any of this throw an error
 };
+
+const addNewHedgehog = () => {
+  const name = $('#name').val();
+  const hoglets = $('#hoglets').val();
+  const allergies = $('#allergies').val();
+  fetch('https://hedgehog-party.herokuapp.com/api/v1/invites', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: name,
+      hoglets: hoglets,
+      allergies: allergies
+    })
+  })
+  .then(response => {
+    return response.json();
+  })
+  .then(jsonResponse => {
+    if(jsonResponse.error) {
+      alert(jsonResponse.error)
+      $('#api-response').append(`<div class=success>Not added successfully</div>`)
+    } else {
+      alert('Hedgehog added successfully');
+      console.log(jsonResponse);
+      $('#api-response').append(`<div class=success>Successfully made request</div>`);
+      getHedgehogs();
+    }
+  })
+  .catch(error => alert(error));
+}
 
 const appendHedgehogs = (hedgehogs) => {
   hedgehogs.forEach(hedgehog => {
@@ -29,18 +59,32 @@ const appendHedgehog = (hedgehog) => {
   `);
 };
 
-const addNewHedgehog = () => {
-  console.log("we are in the addNewHedgehog function");
-};
-
-const unInviteHedgehog = () => {
-  console.log("we are in the unInviteHedgehog function");
+const unInviteHedgehog = (id) => {
+  fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Access-Control-Allow-Methods': 'DELETE'
+    }
+  })
+  .then((response) => {
+    getHedgehogs();
+  })
+  .catch((e) => {
+    console.log(e);
+  })
 };
 
 getHedgehogs();
 
-$('#invite-btn').on('click', addNewHedgehog);
+$('#invite-btn').on('click', (e) => {
+  e.preventDefault();
+  console.log(e);
+  addNewHedgehog();
+});
 
-$('#invited-hedgehogs-info').on('click', '.uninvite-btn', unInviteHedgehog);
+$('#invited-hedgehogs-info').on('click', '.uninvite-btn', (e) => {
+  let id = e.currentTarget.id;
+  unInviteHedgehog(id);
+});
 
 //URL: https://hedgehog-party.herokuapp.com/api/v1/invites
